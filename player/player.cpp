9479662271aedb0d614d3028dec68ec17ff3ef47
asvirido <trotillostudio@gmail.com>
2017-09-13 15:23:36 +0300

    Update smart-contract player

--- player/player.cpp
+++ player/player.cpp
@@ -12,35 +12,24 @@
 
 #include <player/player.hpp>
 
-namespace player
-{
-   using namespace eos;
-
-   void sendMsg(const player::Message& msg)
-   {
-      print(&msg.name);
-      printi(msg.count);
+namespace player  {
+   void sendMsg(const player::Message& msg) {
+      eos::print("Hello, World: ");
+      eos::print(&msg.name);
    }
 }
 
-using namespace player;
+extern "C" {
+    void init() {
 
-extern "C"
-{
-    
-  void init()
-  {
-
-  }
-
-  void apply(uint64_t code, uint64_t action)
-  {
-    if (code == N(player))
-    {
-      if (action == N(hello))
-      { 
-        player::sendMsg(currentMessage< player::Message >());
-      }
     }
-  }
+
+    void apply(uint64_t code, uint64_t action)  {
+        if(code == N(player))  {
+          if(action == N(hello)) {
+            player::sendMsg(eos::currentMessage<player::Message>());
+          }
+        }
+    }
 }
+
--- player/player.hpp
+++ player/player.hpp
@@ -16,7 +16,25 @@ namespace player
 {
    struct Message
    {
-     char 	name = 0;
-     int 	count = 0;
+     char name = 0;
+     int  count = 0;
    };
 }
+
+// struct PACKED(test) {
+// 	int key = 0;
+// };
+
+    // "tables": [
+    //   {
+    //     "table": "test",
+    //     "type": "test",
+    //   }
+    // ]
+
+    // ,{
+    //   "name": "test",
+    //   "fields":{
+    //     "key": "UInt64"
+    //   }
+    //   }
