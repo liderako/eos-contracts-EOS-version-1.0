a4c41743e08b59a66ca7f8616be756a42a740d90
asvirido <trotillostudio@gmail.com>
2017-09-14 20:06:03 +0300

    update player

--- player/player.cpp
+++ player/player.cpp
@@ -36,10 +36,33 @@ return type AccountName
 /* Function 
 int32_t load_i64(AccountName scope, AccountName code, TableName table, void* data,uint32_t  datalen)
 
+scope - the account scope that will be read, must exist in the transaction scopes list
+
+code  - identifies the code that controls write-access to the data
+
+table - the ID/name of the table within the scope/code context to query
+
+data  - location to copy the stored record, should be initialized with the key to get
+
+datalen - the maximum length of data to read, must be greater than sizeof(uint64_t)
 */
+using namespace eos;
 
 namespace player  {
 
+  void  load() {
+
+    table   tmp;
+    int     res;
+
+    res = 0;
+    res = load_i64(currentCode(), currentCode(), N(table), &tmp, sizeof(tmp)); 
+    // test load
+    eos::print("load_i64 == ");
+    eos::print(res);
+    eos::print("\n");
+    /////////////
+  }
 }
 
 extern "C" {
@@ -47,6 +70,8 @@ extern "C" {
     table   tmp;
     int     res;
 
+    res = 0;
+    tmp.key = 0;
     readMessage(&tmp, sizeof(tmp)); // sizeof(tmp) write in tmp
     res = store_i64(currentCode(), N(table), &tmp, sizeof(tmp)); // create or updated 
     
@@ -54,12 +79,11 @@ extern "C" {
     eos::print("store_i64 == ");
     eos::print(res);
     eos::print("\n");
-    //
-    res = load_i64(currentCode(), currentCode(), N(test_table), &alice, sizeof(TestModel) );
     }
 
     void apply(uint64_t code, uint64_t action)  {
-    
+      if (action == N(load))
+        player::load();
     }
 }
 
--- player/player.wast.hpp
+++ player/player.wast.hpp
@@ -1,24 +1,221 @@
 const char* player_wast = R"=====(
 (module
- (type $FUNCSIG$iii (func (param i32 i32) (result i32)))
  (type $FUNCSIG$j (func (result i64)))
- (type $FUNCSIG$ijjii (func (param i64 i64 i32 i32) (result i32)))
+ (type $FUNCSIG$ijjjii (func (param i64 i64 i64 i32 i32) (result i32)))
  (type $FUNCSIG$vi (func (param i32)))
  (type $FUNCSIG$vj (func (param i64)))
+ (type $FUNCSIG$iii (func (param i32 i32) (result i32)))
+ (type $FUNCSIG$ijjii (func (param i64 i64 i32 i32) (result i32)))
  (import "env" "currentCode" (func $currentCode (result i64)))
+ (import "env" "load_i64" (func $load_i64 (param i64 i64 i64 i32 i32) (result i32)))
  (import "env" "printi" (func $printi (param i64)))
  (import "env" "prints" (func $prints (param i32)))
  (import "env" "readMessage" (func $readMessage (param i32 i32) (result i32)))
  (import "env" "store_i64" (func $store_i64 (param i64 i64 i32 i32) (result i32)))
  (table 0 anyfunc)
  (memory $0 1)
- (data (i32.const 4) "@\04\00\00")
+ (data (i32.const 4) "`\04\00\00")
  (data (i32.const 16) "table\00")
- (data (i32.const 32) "store_i64 == \00")
+ (data (i32.const 32) "load_i64 == \00")
  (data (i32.const 48) "\n\00")
+ (data (i32.const 64) "store_i64 == \00")
+ (data (i32.const 80) "load\00")
  (export "memory" (memory $0))
+ (export "_ZN6player4loadEv" (func $_ZN6player4loadEv))
  (export "init" (func $init))
  (export "apply" (func $apply))
+ (func $_ZN6player4loadEv
+  (local $0 i64)
+  (local $1 i64)
+  (local $2 i32)
+  (local $3 i32)
+  (local $4 i64)
+  (local $5 i64)
+  (local $6 i64)
+  (local $7 i64)
+  (local $8 i32)
+  (i32.store offset=4
+   (i32.const 0)
+   (tee_local $8
+    (i32.sub
+     (i32.load offset=4
+      (i32.const 0)
+     )
+     (i32.const 16)
+    )
+   )
+  )
+  (set_local $5
+   (i64.const 0)
+  )
+  (set_local $4
+   (i64.const 59)
+  )
+  (set_local $3
+   (i32.const 16)
+  )
+  (set_local $0
+   (call $currentCode)
+  )
+  (set_local $1
+   (call $currentCode)
+  )
+  (set_local $6
+   (i64.const 0)
+  )
+  (loop $label$0
+   (block $label$1
+    (block $label$2
+     (block $label$3
+      (block $label$4
+       (block $label$5
+        (br_if $label$5
+         (i64.gt_u
+          (get_local $5)
+          (i64.const 4)
+         )
+        )
+        (br_if $label$4
+         (i32.gt_u
+          (i32.and
+           (i32.add
+            (tee_local $2
+             (i32.load8_s
+              (get_local $3)
+             )
+            )
+            (i32.const -97)
+           )
+           (i32.const 255)
+          )
+          (i32.const 25)
+         )
+        )
+        (set_local $2
+         (i32.add
+          (get_local $2)
+          (i32.const 160)
+         )
+        )
+        (br $label$3)
+       )
+       (set_local $7
+        (i64.const 0)
+       )
+       (br_if $label$2
+        (i64.le_u
+         (get_local $5)
+         (i64.const 11)
+        )
+       )
+       (br $label$1)
+      )
+      (set_local $2
+       (select
+        (i32.add
+         (get_local $2)
+         (i32.const 234)
+        )
+        (i32.const 0)
+        (i32.lt_u
+         (i32.and
+          (i32.add
+           (get_local $2)
+           (i32.const -49)
+          )
+          (i32.const 255)
+         )
+         (i32.const 5)
+        )
+       )
+      )
+     )
+     (set_local $7
+      (i64.shr_s
+       (i64.shl
+        (i64.extend_u/i32
+         (get_local $2)
+        )
+        (i64.const 56)
+       )
+       (i64.const 56)
+      )
+     )
+    )
+    (set_local $7
+     (i64.shl
+      (i64.and
+       (get_local $7)
+       (i64.const 31)
+      )
+      (i64.and
+       (get_local $4)
+       (i64.const 4294967295)
+      )
+     )
+    )
+   )
+   (set_local $3
+    (i32.add
+     (get_local $3)
+     (i32.const 1)
+    )
+   )
+   (set_local $5
+    (i64.add
+     (get_local $5)
+     (i64.const 1)
+    )
+   )
+   (set_local $6
+    (i64.or
+     (get_local $7)
+     (get_local $6)
+    )
+   )
+   (br_if $label$0
+    (i64.ne
+     (tee_local $4
+      (i64.add
+       (get_local $4)
+       (i64.const -5)
+      )
+     )
+     (i64.const -6)
+    )
+   )
+  )
+  (set_local $3
+   (call $load_i64
+    (get_local $0)
+    (get_local $1)
+    (get_local $6)
+    (i32.add
+     (get_local $8)
+     (i32.const 8)
+    )
+    (i32.const 8)
+   )
+  )
+  (call $prints
+   (i32.const 32)
+  )
+  (call $printi
+   (i64.extend_s/i32
+    (get_local $3)
+   )
+  )
+  (call $prints
+   (i32.const 48)
+  )
+  (i32.store offset=4
+   (i32.const 0)
+   (i32.add
+    (get_local $8)
+    (i32.const 16)
+   )
+  )
+ )
  (func $init
   (local $0 i64)
   (local $1 i32)
@@ -39,6 +236,13 @@ const char* player_wast = R"=====(
     )
    )
   )
+  (set_local $4
+   (i64.const 0)
+  )
+  (i64.store offset=8
+   (get_local $7)
+   (i64.const 0)
+  )
   (drop
    (call $readMessage
     (i32.add
@@ -47,9 +251,6 @@ const char* player_wast = R"=====(
     )
     (i32.const 8)
    )
-  )
-  (set_local $4
-   (i64.const 0)
   )
   (set_local $3
    (i64.const 59)
@@ -197,7 +398,7 @@ const char* player_wast = R"=====(
    )
   )
   (call $prints
-   (i32.const 32)
+   (i32.const 64)
   )
   (call $printi
    (i64.extend_s/i32
@@ -216,6 +417,155 @@ const char* player_wast = R"=====(
   )
  )
  (func $apply (param $0 i64) (param $1 i64)
+  (local $2 i32)
+  (local $3 i32)
+  (local $4 i64)
+  (local $5 i64)
+  (local $6 i64)
+  (local $7 i64)
+  (set_local $5
+   (i64.const 0)
+  )
+  (set_local $4
+   (i64.const 59)
+  )
+  (set_local $3
+   (i32.const 80)
+  )
+  (set_local $6
+   (i64.const 0)
+  )
+  (loop $label$0
+   (block $label$1
+    (block $label$2
+     (block $label$3
+      (block $label$4
+       (block $label$5
+        (br_if $label$5
+         (i64.gt_u
+          (get_local $5)
+          (i64.const 3)
+         )
+        )
+        (br_if $label$4
+         (i32.gt_u
+          (i32.and
+           (i32.add
+            (tee_local $2
+             (i32.load8_s
+              (get_local $3)
+             )
+            )
+            (i32.const -97)
+           )
+           (i32.const 255)
+          )
+          (i32.const 25)
+         )
+        )
+        (set_local $2
+         (i32.add
+          (get_local $2)
+          (i32.const 160)
+         )
+        )
+        (br $label$3)
+       )
+       (set_local $7
+        (i64.const 0)
+       )
+       (br_if $label$2
+        (i64.le_u
+         (get_local $5)
+         (i64.const 11)
+        )
+       )
+       (br $label$1)
+      )
+      (set_local $2
+       (select
+        (i32.add
+         (get_local $2)
+         (i32.const 234)
+        )
+        (i32.const 0)
+        (i32.lt_u
+         (i32.and
+          (i32.add
+           (get_local $2)
+           (i32.const -49)
+          )
+          (i32.const 255)
+         )
+         (i32.const 5)
+        )
+       )
+      )
+     )
+     (set_local $7
+      (i64.shr_s
+       (i64.shl
+        (i64.extend_u/i32
+         (get_local $2)
+        )
+        (i64.const 56)
+       )
+       (i64.const 56)
+      )
+     )
+    )
+    (set_local $7
+     (i64.shl
+      (i64.and
+       (get_local $7)
+       (i64.const 31)
+      )
+      (i64.and
+       (get_local $4)
+       (i64.const 4294967295)
+      )
+     )
+    )
+   )
+   (set_local $3
+    (i32.add
+     (get_local $3)
+     (i32.const 1)
+    )
+   )
+   (set_local $5
+    (i64.add
+     (get_local $5)
+     (i64.const 1)
+    )
+   )
+   (set_local $6
+    (i64.or
+     (get_local $7)
+     (get_local $6)
+    )
+   )
+   (br_if $label$0
+    (i64.ne
+     (tee_local $4
+      (i64.add
+       (get_local $4)
+       (i64.const -5)
+      )
+     )
+     (i64.const -6)
+    )
+   )
+  )
+  (block $label$6
+   (br_if $label$6
+    (i64.ne
+     (get_local $6)
+     (get_local $1)
+    )
+   )
+   (call $_ZN6player4loadEv)
+  )
  )
 )
 )=====";
