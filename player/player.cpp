2eeb1ad8053d2709f7d92a62bab171da3bf623d9
Antonio <trotillostudio@gmail.com>
2017-09-20 17:28:41 +0300

    Update player.cpp

--- player/player.cpp
+++ player/player.cpp
@@ -15,23 +15,46 @@
 using namespace eos;
 
 namespace player  {
+  
+  // new code
+    void  Store( const Account& account ) {
+      Accounts::store( account, account.name );
+    }
+    void Remove( const Account& account ) {
+     Accounts::remove ( account, account.name ); 
+    }
+    void Win ( AccountName name ) {
+      auto var = Accounts::getAccount( name );
+      
+      var.win += 1;
+      player::Store( var );
+    } // END new code
+  
+// OLD CODE
+//   void  Store( AccountName account, const Account& a ) {
+//     Accounts::store( a, account );
+//   }
+//  void Remove( AccountName account, const Account& a ) {
+//    Accounts::remove( a, account );
+//  }
 
-  void  Store( AccountName account, const Account& a ) {
-    Accounts::store( a, account);
-  }
-  void Remove( AccountName account, const Account& a) {
-    Accounts::remove( a, account);
-  }
 }
 
 extern "C" {
     void init() {
-      AccountName name = currentCode();
-      player::Remove( name, player::Account() );
-      player::Store( name, player::Account() );
+      //AccountName name = currentCode(); old code
+      //player::Remove( name, player::Account() ); old code
+      //player::Store( name, player::Account() ); old code
+      player::Store( player::Account() );
     }
 
-    void apply(uint64_t code, uint64_t action)  {
+    void apply( uint64_t code, uint64_t action )  {
+     // new code
+   //   if ( action == N(remove) )
+   //     player::Remove( getAccount( currentCode()) );
+   //     if ( action == N(win) )
+   //       player::Win( currentCode() ); 
+   // end new code
     }
 }
 
