47f3e0e93a2ef242cfa9e1d66ee94956efab42f1
Antonio <trotillostudio@gmail.com>
2017-09-27 14:45:27 +0300

    Update player.cpp

--- player/player.cpp
+++ player/player.cpp
@@ -6,7 +6,7 @@
 /*   By: asvirido <asvirido@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/17 15:41:12 by asvirido          #+#    #+#             */
-/*   Updated: 2017/09/20 18:18:11 by asvirido         ###   ########.fr       */
+/*   Updated: 2017/09/27 14:45:05 by asvirido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,14 @@
 using namespace eos;
 
 namespace player  {
-  
-    void  Store( const Account& account ) {
-      Accounts::store( account, account.name );
-    }
-    void Remove( const Account& account ) {
-     Accounts::remove ( account, account.name ); 
-    }
-    void Win ( AccountName name ) {
 
-      // requireNotice( name );
-      // requireAuth( name );
+    void  Store( Account account ) {
+      Accounts::store( account.name, N(account), &account, sizeof(account) );
+    }
+
+    void Win ( AccountName name ) {
+      requireNotice( name );
+      requireAuth( name );
       
       auto var = getAccount( name );
       
@@ -40,11 +37,15 @@ extern "C" {
     }
 
     void apply( uint64_t code, uint64_t action )  {
-      if ( action == N(remove) )
-        player::Remove( player::getAccount( currentCode()) );
       if ( action == N(win) )
-        player::Win( currentCode() ); 
-   // end new code
+       player::Win( currentCode() );
+
+
+     /* 
+        AccountName name = currentCode();
+        eos::print("Name:", name , " res N(player):", N(player), " res N(name):", N(name));
+        eos::print(" \n");
+      */
     }
 }
 
