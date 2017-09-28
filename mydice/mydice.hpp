8db44697c7472b8f967aa6f98c88843142010b37
Antonio <trotillostudio@gmail.com>
2017-09-28 17:31:22 +0300

    Update mydice.hpp

--- mydice/mydice.hpp
+++ mydice/mydice.hpp
@@ -6,19 +6,21 @@
 /*   By: asvirido <asvirido@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/17 15:41:12 by asvirido          #+#    #+#             */
-/*   Updated: 2017/09/18 11:37:37 by asvirido         ###   ########.fr       */
+/*   Updated: 2017/09/28 16:38:01 by asvirido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <eoslib/eos.hpp>
 #include <eoslib/db.h>
+#include <eoslib/db.hpp>
 #include <player/player.hpp>
 
 namespace mydice {
-  using namespace player;
+using namespace player;
 
   struct Game {
     AccountName   one;
     AccountName   two;
   };
 }
+
