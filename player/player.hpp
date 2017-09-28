dd53c045d7d59b88d133c617f223c6229e1615ec
Antonio <trotillostudio@gmail.com>
2017-09-28 17:32:44 +0300

    Update player.hpp

--- player/player.hpp
+++ player/player.hpp
@@ -6,18 +6,17 @@
 /*   By: asvirido <asvirido@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/17 15:41:12 by asvirido          #+#    #+#             */
-/*   Updated: 2017/09/27 14:38:40 by asvirido         ###   ########.fr       */
+/*   Updated: 2017/09/28 17:21:47 by asvirido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <eoslib/eos.hpp>
 #include <eoslib/db.h>
 #include <eoslib/db.hpp>
-#include <eoslib/types.h>
 
 namespace player {
-   
-   struct PACKED(Account) {
+
+  struct Account {
 
         AccountName       name;
         uint64_t          win;
@@ -31,22 +30,47 @@ namespace player {
    struct Accounts {
 
     static int32_t get(uint64_t scope, uint64_t code, uint64_t table, void* data, uint32_t len) {
-      return ( load_i64( scope, code, table, data, len) );
+      return ( load_i64( scope, code, table, data, len ) );
     }
-
-    static int32_t store(uint64_t scope, uint64_t table, const void* data, uint32_t len)  {
-      return ( store_i64( scope, table, data, len ) );
+     static int32_t store(AccountName scope, TableName table, void* data, uint32_t len)  {
+       return ( store_i64( scope, table, data, len ) );
+     }
+    static int32_t update(AccountName scope, TableName table, void* data, uint32_t len) {
+      return ( update_i64(scope, table, data, len));
     }
   };
 
-  inline Account getAccount( AccountName owner ) {
+  /*
+    uint64_t  scope == account name;
+    uint64_t  table == name table;
+    void*     data;
+    uint32_t  len == sizeof(data);
+  */
+  void      Store( Account account ) {
+    int res = Accounts::store( account.name, N(account), &account, sizeof(account) );
+    eos::print("Store: ",res, "\n");
+  }
+
+  void      Update( Account account ) {
+    int res = Accounts::update(account.name, N(account), &account, sizeof(account) );
+    eos::print("Update: ", res, "\n");
+  }
+
+  inline    Account getAccount( AccountName owner ) {
     Account   account;
 
-    Accounts::get(owner, currentCode(), N( account ), &account, sizeof(account) );
+    Accounts::get(owner, owner, N( account ), &account, sizeof(account) );
     return ( account );
-   }
+  }
 
-  void    Win( AccountName name );
-  void    Store( Account account ); 
+  void    Win( AccountName name ) {
+    requireNotice( name );
+    requireAuth( name );
+      
+    auto var = getAccount( name );
+      
+    var.win += 1;
+    player::Store( var );
+  //  player::Update( var );
+  }
 }
-
