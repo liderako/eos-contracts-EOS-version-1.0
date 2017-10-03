94ded3fd6c7abd276eee1314cd106328c61704f9
Antonio <trotillostudio@gmail.com>
2017-10-03 17:55:47 +0300

    Update message.cpp

--- message/message.cpp
+++ message/message.cpp
@@ -6,7 +6,7 @@
 /*   By: asvirido <asvirido@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/17 15:41:12 by asvirido          #+#    #+#             */
-/*   Updated: 2017/10/02 21:14:55 by asvirido         ###   ########.fr       */
+/*   Updated: 2017/10/03 17:51:06 by asvirido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,10 @@
 
 using namespace eos;
 
-  // void  Gameplay(const mydice::Game& game) {
-  //   //player::Win(game.one);
-  // }
-
-  //         if ( action == N( game ) ) {
-  //         mydice::Gameplay( currentMessage< mydice::Game >() );
-  //       }
-
 namespace message {
   void output(const message::Input input) { 
-    //char text[2] = {'a','n'};
-    eos::print("Text: == ", &input.text,"\n");
-    eos::print("NUM: == ", input.num, "\n");
-   // prints(input.text);
-   // prints(" \n");
-    //eos::print(input.text);
+    printn(input.text);
+    prints("\n");
   }
 }
 
