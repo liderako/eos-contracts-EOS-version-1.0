6a548c53591bca4b3de83db3e4ebf59944538bc0
Antonio <trotillostudio@gmail.com>
2017-10-03 17:50:54 +0300

    Update message.hpp

--- message/message.hpp
+++ message/message.hpp
@@ -6,20 +6,17 @@
 /*   By: asvirido <asvirido@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/17 15:41:12 by asvirido          #+#    #+#             */
-/*   Updated: 2017/10/02 21:15:47 by asvirido         ###   ########.fr       */
+/*   Updated: 2017/10/03 17:50:25 by asvirido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <eoslib/eos.hpp>
-// #include <eoslib/db.h>
-// #include <eoslib/db.hpp>
 
 namespace message {
+	
+typedef AccountName String;
   
   struct Input {
-    
-    char text = 'a';
-    int num = 1;
-  
+  	String text;
   };
 }
