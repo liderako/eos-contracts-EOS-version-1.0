ad353f4a6d864e66f78b962474cf0b1c131a2134
Antonio <trotillostudio@gmail.com>
2017-10-22 23:28:15 +0300

    Update player.cpp

--- player/player.cpp
+++ player/player.cpp
@@ -6,7 +6,7 @@
 /*   By: asvirido <asvirido@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/17 15:41:12 by asvirido          #+#    #+#             */
-/*   Updated: 2017/09/28 16:15:18 by asvirido         ###   ########.fr       */
+/*   Updated: 2017/10/22 23:07:46 by asvirido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,34 @@
 
 using namespace eos;
 
-extern "C" {
-    void init() {
-      player::Store( player::Account() );
-    }
+namespace player{
 
-    void apply( uint64_t code, uint64_t action )  {
-      if ( action == N(win) )
-       player::Win( currentCode() );
+	void	sendMoney( AccountName name, int countMoney) {
+		Account var = getAccount( name );
+
+		var.money -= countMoney;
+		player::Store( var );
+	}
+
+	void	getMoney( AccountName name, int countMoney) {
+		Account var = getAccount( name );
+
+		var.money += countMoney;
+		player::Store( var );
+	}
+}
+
+extern "C" {
+	void init() {
+		player::Store( player::Account() );
+	}
+
+	void apply( uint64_t code, uint64_t action )  {
+		if ( action == N(sendmoney) ) {
+			player::sendMoney( code, 10 );
+		}
+		else if ( action == N(win) ) {
+			player::getMoney( code, 20 );
+		}
     }
 }
