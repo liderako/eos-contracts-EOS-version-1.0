8040dbcf9df2860abe32a0042a2bcd30483f1ce2
Antonio <trotillostudio@gmail.com>
2017-10-22 23:29:23 +0300

    Update player.hpp

--- player/player.hpp
+++ player/player.hpp
@@ -6,7 +6,7 @@
 /*   By: asvirido <asvirido@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/17 15:41:12 by asvirido          #+#    #+#             */
-/*   Updated: 2017/10/09 13:04:25 by asvirido         ###   ########.fr       */
+/*   Updated: 2017/10/22 23:18:51 by asvirido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,62 +16,36 @@
 
 namespace player {
 
-  struct Account {
+	// Account == data for table
+	struct Account {
+		AccountName		name;
+		uint64_t		money;
 
-        AccountName       name;
-        uint64_t          win;
+		Account() {
+			name = currentCode();
+			money = 100;
+		}
+	};
+   
+   /* 
+    * Use for Table
+    */
+	struct Accounts {
+		static int32_t get(uint64_t scope, uint64_t code, uint64_t table, void* data, uint32_t len)	{
+			return ( load_i64( scope, code, table, data, len ) );
+		}
+		static int32_t store(AccountName scope, TableName table, void* data, uint32_t len)  {
+			return ( store_i64( scope, table, data, len ) );
+		}
+    };
+    inline		Account getAccount( AccountName owner ) {
+    	Account 	account;
 
-        Account() {
-            name = currentCode();
-            win = 0;
-        }
-   };
-
-   struct Accounts {
-
-    static int32_t get(uint64_t scope, uint64_t code, uint64_t table, void* data, uint32_t len) {
-      return ( load_i64( scope, code, table, data, len ) );
+		account.name = owner;
+		Accounts::get(owner, owner, N( account ), &account, sizeof(account) );
+		return ( account );
     }
-     static int32_t store(AccountName scope, TableName table, void* data, uint32_t len)  {
-       return ( store_i64( scope, table, data, len ) );
-     }
-    static int32_t update(AccountName scope, TableName table, void* data, uint32_t len) {
-      return ( update_i64(scope, table, data, len));
-    }
-  };
-
-  /*
-    uint64_t  scope == account name;
-    uint64_t  table == name table;
-    void*     data;
-    uint32_t  len == sizeof(data);
-  */
-  void      Store( Account account ) {
-    int res = Accounts::store( account.name, N(account), &account, sizeof(account) );
-    eos::print("Store: ",res, "\n");
-  }
-
-  void      Update( Account account ) {
-    int res = Accounts::update(account.name, N(account), &account, sizeof(account) );
-    eos::print("Update: ", res, "\n");
-  }
-
-  inline    Account getAccount( AccountName owner ) {
-    Account   account;
-
-    account.name = owner;
-    Accounts::get(owner, currentCode(), N( account ), &account, sizeof(account) );
-    return ( account );
-  }
-
-  void    Win( AccountName name ) {
-    //requireNotice( name );
-    //requireAuth( name );
-      
-    auto var = getAccount( name );
-      
-    var.win += 1;
-    player::Store( var );
-  //  player::Update( var );
-  }
+    void	Store( Account account ) {
+		uint32_t res = Accounts::store( account.name, N(account), &account, sizeof(account) );
+	}
 }
