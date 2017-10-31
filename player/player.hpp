ea2dec2f0b1fc556e6de78408e2f1f8abcde30b4
Antonio <trotillostudio@gmail.com>
2017-10-31 11:53:05 +0200

    Update player.hpp

--- player/player.hpp
+++ player/player.hpp
@@ -6,7 +6,7 @@
 /*   By: asvirido <asvirido@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/17 15:41:12 by asvirido          #+#    #+#             */
-/*   Updated: 2017/10/22 23:18:51 by asvirido         ###   ########.fr       */
+/*   Updated: 2017/10/31 11:51:26 by asvirido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,36 +16,62 @@
 
 namespace player {
 
-	// Account == data for table
-	struct Account {
-		AccountName		name;
-		uint64_t		money;
-
-		Account() {
-			name = currentCode();
-			money = 100;
-		}
-	};
-   
-   /* 
-    * Use for Table
-    */
-	struct Accounts {
-		static int32_t get(uint64_t scope, uint64_t code, uint64_t table, void* data, uint32_t len)	{
+	/*
+	* This struct for work with table.
+	*/
+	struct Table {
+		static int32_t get(uint64_t scope, uint64_t code, uint64_t table, void* data, uint32_t len) {
 			return ( load_i64( scope, code, table, data, len ) );
 		}
 		static int32_t store(AccountName scope, TableName table, void* data, uint32_t len)  {
 			return ( store_i64( scope, table, data, len ) );
 		}
-    };
-    inline		Account getAccount( AccountName owner ) {
-    	Account 	account;
+	}; 
+	
+	/* 
+	*	This class used, how "data table" for table.
+	* 	Account == void *data in struct Table. 
+	*/
+	class Account {
+		private:
+			AccountName		name_;
+			uint64_t		money_;
+		
+		public:
+			Account() {
+				name_ = currentCode();
+				money_ = 100;
+			}
+			Account(AccountName name, uint64_t money = 0) {
+				name_ = name;
+				money_ = money; 
+			}
+			void setName(AccountName name) {
+				name_ = name;
+			}
+			AccountName getName() {
+				return (name_);
+			}
+			void moneyAdd(uint64_t add) {
+				money_ += add;
+			}
+			void moneySub(uint64_t sub) {
+				money_ -= sub;
+			}
+	};
+	/*
+	* This function is used to download data from a table.
+	*/
+	inline		Account getAccount( AccountName owner ) {
+		Account 	account(owner);
 
-		account.name = owner;
-		Accounts::get(owner, owner, N( account ), &account, sizeof(account) );
+		Table::get(owner, owner, N( account ), &account, sizeof(account) );
 		return ( account );
-    }
-    void	Store( Account account ) {
-		uint32_t res = Accounts::store( account.name, N(account), &account, sizeof(account) );
 	}
-}
+	/*
+	* This function is used to set data to a table.
+	*/
+	void	Store( Account account ) {
+		uint32_t res = Table::store( account.getName(), N(account), &account, sizeof(account) );
+	}
+} /* End namespace player */
