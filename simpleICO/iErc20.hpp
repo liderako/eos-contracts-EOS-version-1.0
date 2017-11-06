9398cd188821e571b8fb2020a4539a1a50823ba6
Antonio <trotillostudio@gmail.com>
2017-11-06 14:08:59 +0200

    Update iErc20.hpp

--- simpleICO/iErc20.hpp
+++ simpleICO/iErc20.hpp
@@ -1,3 +1,18 @@
-class iErc20 {
+#include "Iso.hpp";
 
+class iErc20 {
+	public:
+		template<typename Transfer>
+		/*
+		* Send quanity of tokens from adress from to adress to
+		*/
+		virtual void transferFrom(Transfer transfer);
+		/*
+		* Get the total token supply.
+		*/
+		virtual uint64_t totalSupply();
+		/*
+		* Get the account balance of another account with AccountName name.
+		*/
+		virtual	uint64_t balanceOf(AccountName name);	
 };
